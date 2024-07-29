9980041c9c9173a0476466ddea56d887c7ba9c00
TriPham9001 <tri.pham1101@gmail.com>
2024-07-29 17:48:01 +0700

    update

--- src/shared/services/config.service.ts
+++ src/shared/services/config.service.ts
@@ -80,30 +80,6 @@ export class ConfigService {
   get winstonConfig(): winston.LoggerOptions {
     return {
       transports: [
-        new DailyRotateFile({
-          level: 'debug',
-          filename: `./logs/${this.nodeEnv}/debug-%DATE%.log`,
-          datePattern: 'YYYY-MM-DD',
-          zippedArchive: true,
-          maxSize: '20m',
-          maxFiles: '14d',
-          format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.json(),
-          ),
-        }),
-        new DailyRotateFile({
-          level: 'error',
-          filename: `./logs/${this.nodeEnv}/error-%DATE%.log`,
-          datePattern: 'YYYY-MM-DD',
-          zippedArchive: false,
-          maxSize: '20m',
-          maxFiles: '30d',
-          format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.json(),
-          ),
-        }),
         new winston.transports.Console({
           level: 'debug',
           handleExceptions: true,
