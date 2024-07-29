0c5a9e26a176fe371db00c5513c8f414ca185514
TriPham9001 <tri.pham1101@gmail.com>
2024-07-29 22:21:20 +0700

    update

--- src/modules/comic-story/comic-story.module.ts
+++ src/modules/comic-story/comic-story.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ComicStoryService } from './comic-story.service';
 import { ComicStoryController } from './comic-story.controller';
+import { ComicStoryService } from './comic-story.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ComicStoryEntity } from './comic-story.entity';
+import { CqrsModule } from '@nestjs/cqrs';
+import { QueryHandlers } from './handlers';
 
 @Module({
-  providers: [ComicStoryService],
-  controllers: [ComicStoryController]
+  controllers: [ComicStoryController],
+  providers: [ComicStoryService, ...QueryHandlers],
+  imports: [TypeOrmModule.forFeature([ComicStoryEntity]), CqrsModule],
 })
 export class ComicStoryModule {}
