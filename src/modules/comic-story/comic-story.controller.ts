56dcc11635d3e1db1f88abfb8712c776073cd7dd
TriPham9001 <tri.pham1101@gmail.com>
2024-07-30 10:40:29 +0700

    update

--- src/modules/comic-story/comic-story.controller.ts
+++ src/modules/comic-story/comic-story.controller.ts
@@ -1,16 +1,8 @@
-import {
-  Controller,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Query,
-  Version,
-} from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Version } from '@nestjs/common';
 import { ComicStoryService } from './comic-story.service';
 import { PageDto } from 'src/common/dto/page.dto';
 import { ComicStoryDto } from './dtos/comic-story.dto';
 import { SupabaseService } from 'src/shared/services/supabase-s3.service';
-import { ResponseMessage } from 'src/decorators/response-message.decorator';
 
 @Controller('comic-story')
 export class ComicStoryController {
@@ -21,7 +13,6 @@ export class ComicStoryController {
 
   @Version('1')
   @Get()
-  @ResponseMessage('Comic Strories successfully fetched')
   @HttpCode(HttpStatus.OK)
   async getComicStories(): Promise<PageDto<ComicStoryDto>> {
     const comicStories = (await this.comicStoryService.find(
